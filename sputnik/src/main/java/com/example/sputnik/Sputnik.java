8e660f63bd95ae55e8e1f122057fb290f6c9c045
Pelevina <50836278+Pelevina@users.noreply.github.com>
2022-04-20 20:59:48 +0300

    Update Sputnik.java

--- sputnik/src/main/java/com/example/sputnik/Sputnik.java
+++ sputnik/src/main/java/com/example/sputnik/Sputnik.java
@@ -2,16 +2,10 @@ package com.example.sputnik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Sputnik {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Sputnik.class, args);
 	}
-	@GetMapping("/")
-	public void hello() {}
 }
