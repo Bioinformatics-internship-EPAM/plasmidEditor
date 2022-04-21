56779c02d97de258a595cba5f3ff605de6bda67c
Pelevina <50836278+Pelevina@users.noreply.github.com>
2022-04-21 12:37:07 +0300

    Update and rename Sputnik.java to SputnikApplication.java

--- sputnik/src/main/java/com/example/sputnik/SputnikApplication.java
+++ sputnik/src/main/java/com/example/sputnik/SputnikApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sputnik {
+public class SputnikApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Sputnik.class, args);
+		SpringApplication.run(SputnikApplication.class, args);
 	}
 }
