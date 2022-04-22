9408575561c8aad146f0399f3f08ce254382ae10
Evgeniy <Evgeniy@mail.ru>
2022-04-22 11:51:15 +0300

    add naming changes

--- src/main/java/com/plasmideditor/rocket/repos/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.plasmideditor.rocket.repos;
-
-
-import com.plasmideditor.rocket.postgres.GenBankTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<GenBankTable, Long> {
-
-}
---
+++ src/main/java/com/plasmideditor/rocket/repositories/GenBankRepository.java
@@ -0,0 +1,9 @@
+package com.plasmideditor.rocket.repositories;
+
+
+import com.plasmideditor.rocket.entities.GenBankEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenBankRepository extends JpaRepository<GenBankEntity, Long> {
+
+}
