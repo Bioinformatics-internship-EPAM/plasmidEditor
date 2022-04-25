4edb34119ef27cdb0927f23339f5a0f64bc75698
Zakhar Kalachev <zackfnx@yandex.ru>
2022-04-25 15:51:32 +0300

    Add Lombok annotations

--- src/main/java/com/plasmidEditor/sputnik/entities/GenBankEntity.java
+++ src/main/java/com/plasmidEditor/sputnik/entities/GenBankEntity.java
@@ -1,45 +1,21 @@
 package com.plasmidEditor.sputnik.entities;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.EmbeddedId;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 @Table(name = "genbank")
 public class GenBankEntity {
     @EmbeddedId
     private GenBankId id;
-
     private String file;
-
-    public GenBankEntity() {}
-
-    public GenBankEntity(GenBankId id, String file) {
-        this.id = id;
-        this.file = file;
-    }
-
-    public GenBankId getId() {
-        return this.id;
-    }
-
-    public String getFile() {
-        return this.file;
-    }
-
-    public void setId(GenBankId id) {
-        this.id = id;
-    }
-
-    public void setFile(String file) {
-        this.file = file;
-    }
-
-    @Override
-    public String toString() {
-        return "GenBankEntity{" +
-                "id=" + id +
-                ", file='" + file + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/plasmidEditor/sputnik/entities/GenBankId.java
+++ src/main/java/com/plasmidEditor/sputnik/entities/GenBankId.java
@@ -1,56 +1,19 @@
 package com.plasmidEditor.sputnik.entities;
 
+import lombok.*;
+
 import javax.persistence.Embeddable;
 import java.io.Serializable;
-import java.util.Objects;
 
 @Embeddable
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class GenBankId implements Serializable {
     private static final long serialVersionUID = 1L;
     private String accession;
     private String version;
-
-    public GenBankId() {}
-
-    public GenBankId(String accession, String version) {
-        this.accession = accession;
-        this.version = version;
-    }
-
-    public String getAccession() {
-        return this.accession;
-    }
-
-    public String getVersion() {
-        return this.version;
-    }
-
-    public void setAccession(String accession) {
-        this.accession = accession;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        GenBankId id = (GenBankId) object;
-        return accession.equals(id.accession) && version.equals(id.version);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(accession, version);
-    }
-
-    @Override
-    public String toString() {
-        return "GenBankId{" +
-                "accession='" + accession + '\'' +
-                ", version='" + version + '\'' +
-                '}';
-    }
 }
