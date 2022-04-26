663c5cd81ff4300983af786c08a94d7706792062
Zakhar Kalachev <zackfnx@yandex.ru>
2022-04-27 00:55:08 +0300

    Update GenBankService.java

--- src/main/java/com/plasmidEditor/sputnik/services/GenBankService.java
+++ src/main/java/com/plasmidEditor/sputnik/services/GenBankService.java
@@ -1,40 +1,58 @@
 package com.plasmidEditor.sputnik.services;
 
+import com.plasmidEditor.sputnik.GenBankDTO;
 import com.plasmidEditor.sputnik.entities.GenBankEntity;
-import com.plasmidEditor.sputnik.entities.GenBankId;
 import com.plasmidEditor.sputnik.repositories.GenBankRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GenBankService {
     @Autowired
     GenBankRepository repository;
 
-    public GenBankEntity add(GenBankEntity entity) {
-        return repository.saveAndFlush(entity);
+    private final ModelMapper mapper = new ModelMapper();
+
+    public GenBankDTO add(GenBankDTO object) {
+        GenBankEntity entity = mapper.map(object, GenBankEntity.class);
+        return mapper.map(repository.save(entity), GenBankDTO.class);
     }
 
-    public List<GenBankEntity> add(List<GenBankEntity> entities) {
-        return repository.saveAllAndFlush(entities);
+    public GenBankDTO get(String accession, String version) {
+        Optional<GenBankEntity> entity = repository.findByAccessionAndVersion(accession, version);
+        return entity.map(x -> mapper.map(x, GenBankDTO.class)).orElse(null);
     }
 
-    public List<GenBankEntity> getAll() {
-        return repository.findAll();
+    public List<GenBankDTO> getAll() {
+        List<GenBankDTO> objects = new ArrayList<>();
+        List<GenBankEntity> entities = repository.findAll();
+
+        for (GenBankEntity entity : entities) {
+            objects.add(mapper.map(entity, GenBankDTO.class));
+        }
+
+        return objects;
     }
 
-    public GenBankEntity get(String accession, String version) {
-        return repository.findById(new GenBankId(accession, version)).orElse(null);
-    }
+    public GenBankDTO update(GenBankDTO object) {
+        Optional<GenBankEntity> entity = repository.findByAccessionAndVersion(
+                object.getAccession(), object.getVersion());
+        if (entity.isPresent()) {
+            entity.get().setFile(object.getFile());
+            repository.save(entity.get());
+            return mapper.map(entity.get(), GenBankDTO.class);
+        }
 
-    public GenBankEntity update(GenBankEntity entity) {
-        return repository.saveAndFlush(entity);
+        return add(object);
     }
 
     public void delete(String accession, String version) {
-        repository.deleteById(new GenBankId(accession, version));
+        repository.deleteByAccessionAndVersion(accession, version);
     }
 
     public void deleteAll() {
