b4eb8ec484090349dd97e7307626b04cf95e0e7e
Zakhar Kalachev <zackfnx@yandex.ru>
2022-04-27 00:53:49 +0300

    Update GenBankRepository.java

--- src/main/java/com/plasmidEditor/sputnik/repositories/GenBankRepository.java
+++ src/main/java/com/plasmidEditor/sputnik/repositories/GenBankRepository.java
@@ -3,9 +3,14 @@ package com.plasmidEditor.sputnik.repositories;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.plasmidEditor.sputnik.entities.GenBankEntity;
-import com.plasmidEditor.sputnik.entities.GenBankId;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
-public interface GenBankRepository extends JpaRepository<GenBankEntity, GenBankId> {
+public interface GenBankRepository extends JpaRepository<GenBankEntity, Long> {
+    Optional<GenBankEntity> findByAccessionAndVersion(String accession, String version);
 
+    @Transactional
+    void deleteByAccessionAndVersion(String accession, String version);
 }
