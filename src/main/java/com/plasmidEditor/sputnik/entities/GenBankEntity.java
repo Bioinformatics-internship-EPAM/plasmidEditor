3327c4035d4310d5ce3d8d0dafd7225fd079486b
Zakhar Kalachev <zackfnx@yandex.ru>
2022-04-27 00:53:20 +0300

    Update GenBankEntity.java

--- src/main/java/com/plasmidEditor/sputnik/entities/GenBankEntity.java
+++ src/main/java/com/plasmidEditor/sputnik/entities/GenBankEntity.java
@@ -1,21 +1,29 @@
 package com.plasmidEditor.sputnik.entities;
 
 import lombok.*;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.EmbeddedId;
+import javax.persistence.*;
 
 @Entity
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(schema = "genbank", name = "genbanks")
+@Table(
+        schema = "genbank",
+        name = "genbanks",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"accession", "version"})}
+)
 public class GenBankEntity {
-    @EmbeddedId
-    private GenBankId id;
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private Long genbank_id;
+
+    @Column(nullable = false)
+    private String accession;
+
+    @Column(nullable = false)
+    private String version;
 
     @Column(nullable = false)
     private String file;
