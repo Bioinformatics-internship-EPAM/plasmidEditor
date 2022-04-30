46412041b70a428ac659e1caf5d25cc6ea361704
Sophia <sofiya_shalgueva@mail.ru>
2022-04-30 15:41:18 +0300

    Add controllers paclage

---
+++ src/main/java/com/plasmideditor/rocket/genbank/configuration/ApiConstants.java
@@ -0,0 +1,5 @@
+package com.plasmideditor.rocket.genbank.configuration;
+
+public class ApiConstants {
+    public static final String EDIT_FILE_PATH = "/edit";
+}
---
+++ src/main/java/com/plasmideditor/rocket/genbank/controllers/FileEditorController.java
@@ -0,0 +1,12 @@
+package com.plasmideditor.rocket.genbank.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static com.plasmideditor.rocket.genbank.configuration.ApiConstants.EDIT_FILE_PATH;
+
+@Controller
+@RequestMapping(path = EDIT_FILE_PATH)
+public class FileEditorController {
+
+}
