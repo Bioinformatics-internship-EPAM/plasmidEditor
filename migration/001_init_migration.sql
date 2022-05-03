2f4008f595e3cfffbf3dc2fc337f9e99e29f8a50
DmitryVarlamov <varlamov_dmitry99@mail.ru>
2022-05-03 15:02:05 +0300

    Add init migration

---
+++ migration/001_init_migration.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+
+CREATE TABLE genbank.genbanks(genbank_id serial primary key NOT NULL, accession text NOT NULL, version text NOT NULL, file text NOT NULL);
+ALTER TABLE genbank.genbanks ADD CONSTRAINT accession_to_version UNIQUE(accession, version);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+ALTER TABLE genbank.genbanks DROP CONSTRAINT accession_to_version;
+DROP TABLE genbank.genbanks;
+-- +goose StatementEnd
