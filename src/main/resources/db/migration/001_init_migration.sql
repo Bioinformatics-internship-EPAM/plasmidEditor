bc2d6e4a069b33e234590162775d8e1ff24a68d8
DmitryVarlamov <varlamov_dmitry99@mail.ru>
2022-05-04 12:02:13 +0300

    Add create schema statement

--- src/main/resources/db/migration/001_init_migration.sql
+++ src/main/resources/db/migration/001_init_migration.sql
@@ -2,6 +2,7 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 
+CREATE SCHEMA IF NOT EXISTS genbank;
 CREATE TABLE genbank.genbanks(genbank_id serial primary key NOT NULL, accession text NOT NULL, version text NOT NULL, file text NOT NULL);
 ALTER TABLE genbank.genbanks ADD CONSTRAINT accession_to_version UNIQUE(accession, version);
 -- +goose StatementEnd
@@ -11,4 +12,5 @@ ALTER TABLE genbank.genbanks ADD CONSTRAINT accession_to_version UNIQUE(accessio
 SELECT 'down SQL query';
 ALTER TABLE genbank.genbanks DROP CONSTRAINT accession_to_version;
 DROP TABLE genbank.genbanks;
+DROP SCHEMA genbank;
 -- +goose StatementEnd
