b6f84cd2ef577aa9139a5d3a6970aa2cae9f2ed6
Zakhar Kalachev <zackfnx@yandex.ru>
2022-05-04 18:53:52 +0300

    Update GenBankNotFoundException.java

--- src/main/java/com/plasmidEditor/sputnik/exceptions/GenBankNotFoundException.java
+++ src/main/java/com/plasmidEditor/sputnik/exceptions/GenBankNotFoundException.java
@@ -2,10 +2,10 @@ package com.plasmidEditor.sputnik.exceptions;
 
 public class GenBankNotFoundException extends RuntimeException {
     public GenBankNotFoundException(Long id) {
-        super(String.format("GenBank with id %d not found", id));
+        super(String.format("GenBank with id '%d' not found", id));
     }
 
     public GenBankNotFoundException(String accession, String version) {
-        super(String.format("GenBank with accession %s version %s not found", accession, version));
+        super(String.format("GenBank with accession '%s' version '%s' not found", accession, version));
     }
 }
