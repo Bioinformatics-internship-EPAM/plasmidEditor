1f20f290200aa55c38cefd3b0f20b6ff95f1b12c
Sophia <sofiya_shalgueva@mail.ru>
2022-05-09 20:07:25 +0300

    Rebase on Anya's branch

---
+++ src/main/java/com/plasmideditor/rocket/genbank/services/exceptions/SequenceValidationException.java
@@ -0,0 +1,24 @@
+package com.plasmideditor.rocket.genbank.services.exceptions;
+
+public class SequenceValidationException extends Exception {
+    private ExpectedType type;
+
+    public SequenceValidationException(String msg, ExpectedType type) {
+        super(msg);
+        this.type = type;
+    }
+    public SequenceValidationException(String msg, Throwable e, ExpectedType type) {
+        super(msg, e);
+        this.type = type;
+    }
+
+    public ExpectedType getType() {
+        return type;
+    }
+
+    public enum ExpectedType {
+        DNA,
+        PROTEIN
+    }
+}
+
