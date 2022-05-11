7712354a42f5bc1f58458dfbf3126676b9e70ede
Zakhar Kalachev <zackfnx@yandex.ru>
2022-05-11 22:19:45 +0300

    Update GenBankEditor.java

--- src/main/java/com/plasmidEditor/sputnik/GenBankEditor.java
+++ src/main/java/com/plasmidEditor/sputnik/GenBankEditor.java
@@ -13,23 +13,57 @@ import java.io.StringReader;
 import java.lang.reflect.InvocationTargetException;
 
 public class GenBankEditor {
+    private enum EditorType {
+        ADD_METHOD,
+        MODIFY_METHOD,
+        CUT_METHOD
+    }
+
     public <S extends AbstractSequence<C>, C extends Compound> S add(
-            String sequence, int position, String genbankContent, Class<S> sequenceClass) {
+            String subSequence, int position, String genbankContent, Class<S> sequenceClass) {
+        return editorMethod(subSequence, position, genbankContent, sequenceClass, EditorType.ADD_METHOD);
+    }
+
+    public <S extends AbstractSequence<C>, C extends Compound> S modify(
+            String subSequence, int position, String genbankContent, Class<S> sequenceClass) {
+        return editorMethod(subSequence, position, genbankContent, sequenceClass, EditorType.MODIFY_METHOD);
+    }
+
+    public <S extends AbstractSequence<C>, C extends Compound> S cut(
+            String subSequence, int position, String genbankContent, Class<S> sequenceClass) {
+        return editorMethod(subSequence, position, genbankContent, sequenceClass, EditorType.CUT_METHOD);
+    }
+
+    private <S extends AbstractSequence<C>, C extends Compound> S editorMethod
+            (String subSequence, int position, String genbankContent, Class<S> sequenceClass, EditorType editorType) {
         BufferedReader bufferedReader = new BufferedReader(new StringReader(genbankContent));
         GenbankSequenceParser<S, C> parser = new GenbankSequenceParser<>();
 
         StringBuilder sequenceBuilder = new StringBuilder(parser.getSequence(bufferedReader, 0));
-        sequenceBuilder.insert(position - 1, sequence);
 
+        switch (editorType) {
+            case ADD_METHOD: sequenceBuilder.insert(position - 1, subSequence); break;
+            case MODIFY_METHOD: sequenceBuilder.replace(position - 1, subSequence.length(), subSequence); break;
+            case CUT_METHOD: /* TODO cut method */ break;
+        }
+
+        S sequence = createNewSequenceInstance(sequenceBuilder.toString(), parser, sequenceClass);
+        adjustLocations(sequence, position, subSequence.length(), editorType);
+
+        return sequence;
+    }
+
+    private <S extends AbstractSequence<C>, C extends Compound> S createNewSequenceInstance(
+            String sequence, GenbankSequenceParser<S,C> parser, Class<S> sequenceClass) {
         S newSequence;
         try {
-            newSequence = sequenceClass.getConstructor(String.class).newInstance(sequenceBuilder.toString());
+            newSequence = sequenceClass.getConstructor(String.class).newInstance(sequence);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new NewSequenceInstanceException("Failed to create an instance", e);
         }
 
         parser.getSequenceHeaderParser().parseHeader(parser.getHeader(), newSequence);
-        adjustLocations(newSequence, position, sequence.length(), true);
+        parser.parseFeatures(newSequence);
 
         DBReferenceInfo dbQualifier = parser.getDatabaseReferences().get("db_xref").get(0);
         if (dbQualifier != null) {
@@ -37,13 +71,11 @@ public class GenBankEditor {
                     new TaxonomyID(dbQualifier.getDatabase() + ":" + dbQualifier.getId(), DataSource.GENBANK));
         }
 
-        // TODO set accession
-
         return newSequence;
     }
 
-    private <S extends AbstractSequence<C>, C extends Compound> void adjustLocations(
-            S sequence, int position, int offset, boolean increase) {
-        // TODO adjust locations
+    private <S extends AbstractSequence<C>, C extends Compound> void adjustLocations
+            (S sequence, int position, int offset, EditorType editorType) {
+        // TODO adjust location method (add, modify or cut)
     }
 }
