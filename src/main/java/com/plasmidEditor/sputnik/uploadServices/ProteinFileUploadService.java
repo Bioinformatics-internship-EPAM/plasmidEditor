8baea24bca33ffbe686fa24e3dec3c3d5556d7ea
Pelevina <50836278+Pelevina@users.noreply.github.com>
2022-05-12 10:40:02 +0300

    Замена MultipartFile на InputStream

--- src/main/java/com/plasmidEditor/sputnik/uploadServices/ProteinFileUploadService.java
+++ src/main/java/com/plasmidEditor/sputnik/uploadServices/ProteinFileUploadService.java
@@ -1,12 +1,13 @@
 package com.plasmidEditor.sputnik.uploadServices;
 
+import java.io.InputStream;
+
 import org.biojava.nbio.core.sequence.ProteinSequence;
-import org.springframework.web.multipart.MultipartFile;
 
 public class ProteinFileUploadService implements FileUploadService<ProteinSequence> {
 
 	@Override
-	public void upload(MultipartFile file) {
+	public void upload(InputStream file) {
 		
 	}
 
