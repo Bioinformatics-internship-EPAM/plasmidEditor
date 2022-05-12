f14642757be410ae9b7f1a70f32078cec79b5ff0
Zhukova Ksenia <zhukova.ksenia2000@gmail.com>
2022-05-13 02:52:31 +0300

    Fix controller, add exceptions handling in JSON format

--- src/main/java/com/plasmidEditor/sputnik/controllers/GenbankFileDownloadController.java
+++ src/main/java/com/plasmidEditor/sputnik/controllers/GenbankFileDownloadController.java
@@ -2,14 +2,12 @@ package com.plasmidEditor.sputnik.controllers;
 
 import com.plasmidEditor.sputnik.download.DNAFileDownloadService;
 import com.plasmidEditor.sputnik.download.ProteinFileDownloadService;
-import com.plasmidEditor.sputnik.exceptions.DownloadGenbankFileException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value="/")
 public class GenbankFileDownloadController {
     private final DNAFileDownloadService dnaFileDownloadService;
     private final ProteinFileDownloadService proteinFileDownloadService;
@@ -26,7 +24,6 @@ public class GenbankFileDownloadController {
     )
     public ResponseEntity<String> downloadDNAFile(@RequestParam String accession,
                                              @RequestParam(defaultValue = "latest") String version) {
-        System.out.println("accession: " + accession + " version: " + version);
         String dnaFile = dnaFileDownloadService.downloadFileAsString(accession, version);
         return ResponseEntity.ok() //status 200
                 .header("Download-Status","File was successfully downloaded")
@@ -42,12 +39,5 @@ public class GenbankFileDownloadController {
         return ResponseEntity.ok() //status 200
                 .header("Download-Status","File was successfully downloaded")
                 .body(proteinFile);
-    }
-
-    @ExceptionHandler(DownloadGenbankFileException.class)
-    public ResponseEntity<String> handleException(DownloadGenbankFileException e) {
-        return ResponseEntity.badRequest() //status 400
-                .header("Download-Status","Fail to download file")
-                .body("Reason: " + e.getMessage());
     }
 }
---
+++ src/main/java/com/plasmidEditor/sputnik/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.plasmidEditor.sputnik.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(DownloadGenbankFileException.class)
+    protected ResponseEntity<DownloadGenbankFileJsonException> handleDownloadGenbankFileException(DownloadGenbankFileException e) {
+        return new ResponseEntity<>(
+                new DownloadGenbankFileJsonException("Fail to download file", e.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class DownloadGenbankFileJsonException {
+        private String downloadStatus;
+        private String errorMessage;
+    }
+}
