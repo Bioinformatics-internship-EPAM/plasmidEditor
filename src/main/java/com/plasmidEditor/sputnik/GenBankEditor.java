296d11ecb23c31b07e28406024f0ac1b3dd92d35
Venjilin <chapter1479@gmail.com>
2022-05-15 02:06:55 +0300

    function adjustLocation

--- src/main/java/com/plasmidEditor/sputnik/GenBankEditor.java
+++ src/main/java/com/plasmidEditor/sputnik/GenBankEditor.java
@@ -1,10 +1,12 @@
 package com.plasmidEditor.sputnik;
 
 import com.plasmidEditor.sputnik.exceptions.NewSequenceInstanceException;
+import lombok.val;
 import org.biojava.nbio.core.sequence.DataSource;
 import org.biojava.nbio.core.sequence.TaxonomyID;
 import org.biojava.nbio.core.sequence.features.DBReferenceInfo;
 import org.biojava.nbio.core.sequence.io.GenbankSequenceParser;
+import org.biojava.nbio.core.sequence.location.SimpleLocation;
 import org.biojava.nbio.core.sequence.template.AbstractSequence;
 import org.biojava.nbio.core.sequence.template.Compound;
 
@@ -36,6 +38,7 @@ public class GenBankEditor {
 
     private <S extends AbstractSequence<C>, C extends Compound> S editorMethod
             (String subSequence, int position, String genbankContent, Class<S> sequenceClass, EditorType editorType) {
+        int offset = subSequence.length();
         BufferedReader bufferedReader = new BufferedReader(new StringReader(genbankContent));
         GenbankSequenceParser<S, C> parser = new GenbankSequenceParser<>();
 
@@ -43,12 +46,12 @@ public class GenBankEditor {
 
         switch (editorType) {
             case ADD_METHOD: sequenceBuilder.insert(position - 1, subSequence); break;
-            case MODIFY_METHOD: sequenceBuilder.replace(position - 1, subSequence.length(), subSequence); break;
-            case CUT_METHOD: /* TODO cut method */ break;
+            case MODIFY_METHOD: sequenceBuilder.replace(position - 1, position - 1 + offset, subSequence); break;
+            case CUT_METHOD: sequenceBuilder.delete(position - 1, position - 1 + offset); break;
         }
 
         S sequence = createNewSequenceInstance(sequenceBuilder.toString(), parser, sequenceClass);
-        adjustLocations(sequence, position, subSequence.length(), editorType);
+        adjustLocations(sequence, position, offset, editorType);
 
         return sequence;
     }
@@ -76,6 +79,49 @@ public class GenBankEditor {
 
     private <S extends AbstractSequence<C>, C extends Compound> void adjustLocations
             (S sequence, int position, int offset, EditorType editorType) {
-        // TODO adjust location method (add, modify or cut)
+        switch (editorType) {
+            case MODIFY_METHOD:
+                return;
+            case ADD_METHOD:
+                for (var f : sequence.getFeatures()) {
+                    int startPosition = f.getLocations().getStart().getPosition();
+                    int endPosition = f.getLocations().getEnd().getPosition();
+
+                    if (position < startPosition) {
+                        startPosition += offset;
+                        endPosition += offset;
+                    } else if (position < endPosition) {
+                        endPosition += offset;
+                    }
+                    f.setLocation(new SimpleLocation(startPosition, endPosition));
+                }
+                break;
+            case CUT_METHOD:
+                for (var f: sequence.getFeatures()) {
+                    int startPosition = f.getLocations().getStart().getPosition();
+                    int endPosition = f.getLocations().getEnd().getPosition();
+
+                    if (position < startPosition) {
+                        if (position + offset < startPosition) {
+                            startPosition -= offset;
+                            endPosition -= offset;
+                        } else if (position + offset < endPosition) {
+                            startPosition = position;
+                            endPosition -= offset;
+                        } else {
+                            startPosition = endPosition = position;
+                            // TODO: cut the feature
+                        }
+                    } else if (position < endPosition) {
+                        if (position + offset < endPosition)
+                            endPosition -= offset;
+                        else
+                            endPosition = position;
+                    }
+
+                    f.setLocation(new SimpleLocation(startPosition, endPosition));
+                }
+                break;
+        }
     }
 }
