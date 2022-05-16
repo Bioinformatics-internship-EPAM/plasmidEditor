dcca4d24eb15bfe7de77badf45f6fa98e7270008
Pelevina <50836278+Pelevina@users.noreply.github.com>
2022-05-16 23:01:38 +0300

    Update FileUploadService.java

--- src/main/java/com/plasmidEditor/sputnik/uploadServices/FileUploadService.java
+++ src/main/java/com/plasmidEditor/sputnik/uploadServices/FileUploadService.java
@@ -2,6 +2,8 @@ package com.plasmidEditor.sputnik.uploadServices;
 
 import java.io.InputStream;
 
+import com.plasmidEditor.sputnik.exceptions.FileUploadException;
+
 public interface FileUploadService<T> {
-	public void upload(InputStream file);
+	public void upload(InputStream inputStream) throws FileUploadException;
 }
