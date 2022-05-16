4ac369fea8a22884bb16aeded079d734439ea6d7
Pelevina <50836278+Pelevina@users.noreply.github.com>
2022-05-16 14:24:55 +0300

    Create FileUploadException.java

---
+++ src/main/java/com/plasmidEditor/sputnik/exceptions/FileUploadException.java
@@ -0,0 +1,6 @@
+package com.plasmidEditor.sputnik.exceptions;
+
+public class FileUploadException extends Exception {
+  public FileUploadException(String message, Throwable cause) {
+    super(message, cause);
+  }
