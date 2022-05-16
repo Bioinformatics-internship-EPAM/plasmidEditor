56e5f891339a9e217e549a581cb04d4e1bf17552
lyzhib <74363315+lyzhib@users.noreply.github.com>
2022-05-16 14:54:21 +0300

    Update ProteinFileUploadService.java

--- src/main/java/com/plasmidEditor/sputnik/uploadServices/ProteinFileUploadService.java
+++ src/main/java/com/plasmidEditor/sputnik/uploadServices/ProteinFileUploadService.java
@@ -1,51 +1,46 @@
 package com.plasmidEditor.sputnik.uploadServices;
 
-import java.io.InputStream;
-
+import com.plasmidEditor.sputnik.FileProteinGenbankManager;
+import com.plasmidEditor.sputnik.exceptions.FileUploadException;
+import com.plasmidEditor.sputnik.services.GenBankServiceImpl;
 import org.biojava.nbio.core.sequence.ProteinSequence;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.util.List;
+import java.io.InputStream;
 
 public class ProteinFileUploadService implements FileUploadService<ProteinSequence> {
 
+	private final GenBankServiceImpl genBankServiceImpl;
+	
+	@Autowired
+	public ProteinFileUploadService(GenBankServiceImpl genBankServiceImpl) {
+		this.genBankServiceImpl = genBankServiceImpl;
+	}
+
 	@Override
-    public void uploadFile(InputStream inputStream) throws FileEditorUploadException {
-        List<ProteinSequence> sequenceList;
+	public void uploadFile(InputStream inputStream) throws FileUploadException {
+		ProteinSequence sequence;
 
-        try {
-            File file = File.createTempFile("protein", ".tmp");
-            file.deleteOnExit();
-            // Write to tmp file to get accession and version
-            multipartFile.transferTo(file);
-
-            // If possible to read then the format is correct
-            sequenceList = new FileProteinGenbankManager().readSequence(file.getAbsolutePath());
-            validateListSize(sequenceList);
-        } catch (Exception e) {
-            throw new FileEditorUploadException(e.getMessage(), e);
-        }
-
-        for (ProteinSequence sequence : sequenceList) {
-            String accession = sequence.getAccession().getID();
-            int version = sequence.getAccession().getVersion();
-
-//             if (genBankService.getByAccessionVersion(accession, version) == null) {
-//                 genBankService.upload(accession, version, new String(multipartFile.getBytes()));
-//             }
-            System.out.println("Upload " + accession + "." + version);
-        }
-    }
-
-    private void validateListSize(List<ProteinSequence> sequences) throws Exception {
-        switch (sequences.size()) {
-            case 0:
-                throw new Exception("File has invalid format.");
-            case 1:
-                break;
-            default:
-                throw new Exception("Upload " +
-                        sequences.size() + " sequences at once. " +
-                        "Upload only one sequence per file."
-                );
-        }
+        	try {
+            		File file = File.createTempFile("Protein", ".tmp");
+            		file.deleteOnExit();
+            		inputStream.transferTo(file);
+            		sequence = new FileProteinGenbankManager().readSequense(file.getAbsolutePath());
+			if (sequense instanceof ProteinSequence) {
+        			String accession = sequence.getAccession().getID();
+        			int version = sequence.getAccession().getVersion();
+				String content = new ReaderUtils().readStringFromFile(file.getAbsolutePath());
+				genBankServiceImpl.save(new GenBankDTO(accession, String.valueOf(version), content));
+			}
+			else {
+				throw new NullPointerException();
+			}
+        	} catch (Exception e) {
+            		throw new FileUploadException(e.getMessage(), e);
+        	}
+		
     }
 
 }
