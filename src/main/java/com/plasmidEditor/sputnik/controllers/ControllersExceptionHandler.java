64949a3dc32231f27e9a8d212a52da4888846410
Pelevina <50836278+Pelevina@users.noreply.github.com>
2022-05-17 00:39:54 +0300

    Update ControllersExceptionHandler.java

--- src/main/java/com/plasmidEditor/sputnik/controllers/ControllersExceptionHandler.java
+++ src/main/java/com/plasmidEditor/sputnik/controllers/ControllersExceptionHandler.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ControllersExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<JsonError> handleException(Exception e) {
-		return ResponseEntity.badRequest().body(new JsonError("invalid file", e.getMessage()));
+		return ResponseEntity.badRequest().body(new ErrorResponseBody("invalid file", e.getMessage()));
 	}
 }
