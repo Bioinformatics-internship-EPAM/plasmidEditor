2f04e39100deb7b816ec1ec80db8d18bbcc08cc8
Pelevina <50836278+Pelevina@users.noreply.github.com>
2022-05-16 23:00:10 +0300

    Избавление от дублирующегося кода

--- src/main/java/com/plasmidEditor/sputnik/uploadServices/ProteinFileUploadService.java
+++ src/main/java/com/plasmidEditor/sputnik/uploadServices/ProteinFileUploadService.java
@@ -3,11 +3,11 @@ package com.plasmidEditor.sputnik.uploadServices;
 import com.plasmidEditor.sputnik.FileProteinGenbankManager;
 import com.plasmidEditor.sputnik.exceptions.FileUploadException;
 import com.plasmidEditor.sputnik.services.GenBankServiceImpl;
+
 import org.biojava.nbio.core.sequence.ProteinSequence;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
-import java.util.List;
 import java.io.InputStream;
 
 public class ProteinFileUploadService implements FileUploadService<ProteinSequence> {
@@ -22,21 +22,12 @@ public class ProteinFileUploadService implements FileUploadService<ProteinSequen
 	@Override
 	public void uploadFile(InputStream inputStream) throws FileUploadException {
 		ProteinSequence sequence;
+		FileUploadServiceUtils<ProteinSequence> serviceUtils = new FileUploadServiceUtils<>();
 
         	try {
-            		File file = File.createTempFile("Protein", ".tmp");
-            		file.deleteOnExit();
-            		inputStream.transferTo(file);
-            		sequence = new FileProteinGenbankManager().readSequense(file.getAbsolutePath());
-			if (sequenсe instanceof ProteinSequence) {
-        			String accession = sequence.getAccession().getID();
-        			int version = sequence.getAccession().getVersion();
-				String content = new ReaderUtils().readStringFromFile(file.getAbsolutePath());
-				genBankServiceImpl.save(new GenBankDTO(accession, String.valueOf(version), content));
-			}
-			else {
-				throw new NullPointerException();
-			}
+            		File file = serviceUtils.writeToTmpFile(inputStream);
+            		sequence = new FileProteinGenbankManager().readSequence(file.getAbsolutePath());
+            		serviceUtils.saveSequenceToDB(sequence, file, genBankServiceImpl);
         	} catch (Exception e) {
             		throw new FileUploadException(e.getMessage(), e);
         	}
