66415cb7281841c3e1837e1604c4251be5c484b0
Zakhar Kalachev <zackfnx@yandex.ru>
2022-05-16 20:02:46 +0300

    Create SequenceClassException.java

---
+++ src/main/java/com/plasmidEditor/sputnik/exceptions/SequenceClassException.java
@@ -0,0 +1,7 @@
+package com.plasmidEditor.sputnik.exceptions;
+
+public class SequenceClassException extends RuntimeException {
+    public SequenceClassException(String message) {
+        super(message);
+    }
+}
