d9ddfc85ac5b430972e9528ab6a9d06ef2e90d6a
Pelevina <50836278+Pelevina@users.noreply.github.com>
2022-05-16 14:15:41 +0300

    Update DNAFileUploadService.java

--- src/main/java/com/plasmidEditor/sputnik/uploadServices/DNAFileUploadService.java
+++ src/main/java/com/plasmidEditor/sputnik/uploadServices/DNAFileUploadService.java
@@ -1,55 +1,46 @@
 package com.plasmidEditor.sputnik.uploadServices;
 
-import com.plasmideditor.rocket.genbank.io.protein.FileDNAGenbankManager;
-import com.plasmideditor.rocket.web.service.exceptions.FileEditorUploadException;
+import com.plasmideditor.sputnik.FileDNAGenbankManager;
+import com.plasmideditor.sputnik.exceptions.FileUploadException;
+import com.plasmideditor.sputnik.services.GenBankServiceImpl;
 import org.biojava.nbio.core.sequence.ProteinSequence;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
 import java.util.List;
-import java.io.InputStream
+import java.io.InputStream;
 
 public class DNAFileUploadService implements FileUploadService<DNASequence> {
 
+	private final GenBankServiceImpl genBankServiceImpl;
+	
+	@Autowired
+	public DNAFileUploadService(GenBankServiceImpl genBankServiceImpl) {
+		this.genBankServiceImpl = genBankServiceImpl;
+	}
+
 	@Override
-    public void uploadFile(InputStream inputstream) throws FileEditorUploadException {
-        List<DNASequence> sequenceList;
+	public void uploadFile(InputStream inputStream) throws FileUploadException {
+		DNASequence sequence;
 
-        try {
-            File file = File.createTempFile("dna", ".tmp");
-            file.deleteOnExit();
-            // Write to tmp file to get accession and version
-            inputStream.transferTo(file);
-
-            // If possible to read then the format is correct
-            sequenceList = new FileDNAGenbankManager().readSequense(file.getAbsolutePath());
-            validateListSize(sequenceList);
-        } catch (Exception e) {
-            throw new FileEditorUploadException(e.getMessage(), e);
-        }
-
-        for (DNASequence sequence : sequenceList) {
-            String accession = sequence.getAccession().getID();
-            int version = sequence.getAccession().getVersion();
-
-//             if (genBankService.getByAccessionVersion(accession, version) == null) {
-//                 genBankService.upload(accession, version, new String(multipartFile.getBytes()));
-//             }
-            System.out.println("Upload " + accession + "." + version);
-        }
-    }
-
-    private void validateListSize(List<DNASequence> sequences) throws Exception {
-        switch (sequences.size()) {
-            case 0:
-                throw new Exception("File has invalid format.");
-            case 1:
-                break;
-            default:
-                throw new Exception("Upload " +
-                        sequences.size() + " sequences at once. " +
-                        "Upload only one sequence per file."
-                );
-        }
+        	try {
+            		File file = File.createTempFile("dna", ".tmp");
+            		file.deleteOnExit();
+            		inputStream.transferTo(file);
+            		sequence = new FileDNAGenbankManager().readSequense(file.getAbsolutePath());
+			if (sequense instanceof DNASequence) {
+        			String accession = sequence.getAccession().getID();
+        			int version = sequence.getAccession().getVersion();
+				String content = new ReaderUtils().readStringFromFile(file.getAbsolutePath());
+				genBankServiceImpl.save(new GenBankDTO(accession, String.valueOf(version), content));
+			}
+			else {
+				throw new NullPointerException();
+			}
+        	} catch (Exception e) {
+            		throw new FileUploadException(e.getMessage(), e);
+        	}
+		
     }
 
 }
