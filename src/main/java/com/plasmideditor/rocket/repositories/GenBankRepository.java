5ba1866c653974f833321a3384a0da3322bdc949
Sophia <sofiya_shalgueva@mail.ru>
2022-05-17 15:26:28 +0300

    Rebasing on database mr

--- src/main/java/com/plasmideditor/rocket/repositories/GenBankRepository.java
+++ src/main/java/com/plasmideditor/rocket/repositories/GenBankRepository.java
@@ -4,8 +4,11 @@ package com.plasmideditor.rocket.repositories;
 import com.plasmideditor.rocket.entities.GenBankEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GenBankRepository extends JpaRepository<GenBankEntity, Long> {
     Optional<GenBankEntity> findByAccessionAndVersion(String accession, String version);
+
+    List<GenBankEntity> findGenBankEntitiesByAccession(String accession);
 }
