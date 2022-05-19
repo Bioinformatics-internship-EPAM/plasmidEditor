26e4e52d2b3729974565f5063b955e43a4ce3bcb
Pelevina <50836278+Pelevina@users.noreply.github.com>
2022-05-19 16:51:51 +0300

    Add files via upload

---
+++ src/main/java/com/plasmidEditor/sputnik/exceptions/RepeatFileUploadingException.java
@@ -0,0 +1,7 @@
+package com.plasmidEditor.sputnik.exceptions;
+
+public class RepeatFileUploadingException extends RuntimeException {
+	public RepeatFileUploadingException(String message) {
+		super(message);
+	}
+}
