a78e17e771a44df8e143aae219afe3d947598d13
Pelevina <50836278+Pelevina@users.noreply.github.com>
2022-05-30 15:34:25 +0300

    Изменение типа второго аргумента метода
    saveSequenceToDB

--- src/main/java/com/plasmidEditor/sputnik/uploadServices/FileUploadService.java
+++ src/main/java/com/plasmidEditor/sputnik/uploadServices/FileUploadService.java
@@ -15,14 +15,14 @@ public abstract class FileUploadService<T extends AbstractSequence<?>> {
     
     public abstract void upload(InputStream inputStream) throws FileUploadException;
 
-    protected void saveSequenceToDB(T sequence, InputStream inputStream) throws IllegalArgumentException, 
+    protected void saveSequenceToDB(T sequence, byte[] cachedInputStream) throws IllegalArgumentException, 
     IOException, GenBankNotFoundException, RepeatFileUploadingException {
         String accession = sequence.getAccession().getID();
         String version = String.valueOf(sequence.getAccession().getVersion());
         if (genBankService.isExists(accession, version)) {
             throw new RepeatFileUploadingException(accession, version);
         } else {
-            String content = new String(inputStream.readAllBytes());
+        	String content = new String(cachedInputStream);
             genBankService.save(new GenBankDTO(accession, version, content));
         }
     }
